310d670b7c4f3be7cb03b2aa148a5bfd028f8b4d
Dan Riding <doonyby@gmail.com>
2016-11-20 22:45:50 -0700

    added waiting div

--- module.js
+++ module.js
@@ -1,18 +1,20 @@
 angular.module('myApp', ['ngMessages'])
 	.controller('myCtrl', function($http, $sce) {
 		var vm = this;
-		
+		vm.searchingDiv = false;
 		vm.trustSrc = function(src) {
 		  return $sce.trustAsResourceUrl(src);
 		};
 
 		vm.formSubmit = function(tag) {
-			vm.tag = tag;
+			vm.results = null;
+			document.getElementById('inputText').value = '';
+			vm.searchingDiv = true;
 			var url = "https://api.flickr.com/services/rest";
 			var request = {
 			    method: 'flickr.photos.search',
 			    api_key: '5bab27af3a0be31e242ef48d19c5d96a',
-			    tags: vm.tag,
+			    tags: tag,
 			    format: 'json',
 			    nojsoncallback: 1
 			};
@@ -24,8 +26,12 @@ angular.module('myApp', ['ngMessages'])
 			})
 			.then(function(response) {
 				vm.results = response.data.photos.photo;
+				vm.tagger = tag;
 			}, function(response) {
 				console.log('error: ', response);
+			})
+			.finally(function() {
+				vm.searchingDiv = false;
 			});
 		}
 	});
